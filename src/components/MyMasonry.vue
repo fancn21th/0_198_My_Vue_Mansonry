a9c5b1a96ac2da2a6ad5b1b5176c3fb0e1971a7e
fancn21th <fancn21th@gmail.com>
2022-06-19 17:19:19 +0800

    different height

--- src/components/MyMasonry.vue
+++ src/components/MyMasonry.vue
@@ -1,12 +1,12 @@
 <template>
   <div ref="grid">
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
+    <div class="grid-item height-5">1</div>
+    <div class="grid-item height-10">2</div>
+    <div class="grid-item height-20">3</div>
+    <div class="grid-item height-30">4</div>
+    <div class="grid-item height-40">5</div>
+    <div class="grid-item height-30">6</div>
+    <div class="grid-item height-20">7</div>
   </div>
 </template>
 
@@ -25,6 +25,10 @@ export default {
 </script>
 
 <style>
-.grid-item { width: 25%; min-height: 100px; border: 1px dashed #ccc }
-
+.grid-item { width: 25%; border: 1px dashed #ccc; display: flex; align-items: center; justify-content: center; }
+.height-5 { height: 5rem }
+.height-10 { height: 10rem }
+.height-20 { height: 20rem }
+.height-30 { height: 30rem }
+.height-40 { height: 40rem }
 </style>
